a061286aba1ceeac6635a6e7dc0aae69d382f809
Ivan Studenikin <515516@gmail.com>
2016-05-29 12:22:58 +0400

    fixed: the Main.OutputDeck() method

--- SortDeck/Program.cs
+++ SortDeck/Program.cs
@@ -14,14 +14,10 @@ namespace SortDeck
                 new Card ("Кельн", "Москва")
             };
 
-            OutputDeck(deck);
-            Console.WriteLine();
-
             var sorter = new DeckSorter(deck);
             sorter.Sort();
 
             OutputDeck(deck);
-
             Console.Read();
         }
 
@@ -29,7 +25,7 @@ namespace SortDeck
         {
             foreach (var card in deck)
             {
-                Console.Write(string.Format("{0} > {1}, ", card.From, card.To));
+                Console.WriteLine("{0} > {1}", card.From, card.To);
             }
         }
     }
