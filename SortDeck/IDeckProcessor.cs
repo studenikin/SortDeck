1c9242a57baa1250782dd4d3c3aa0c70522ecbd5
Ivan Studenikin <515516@gmail.com>
2016-05-29 12:27:38 +0400

    ICardProcessor: annotation added

--- SortDeck/IDeckProcessor.cs
+++ SortDeck/IDeckProcessor.cs
@@ -1,5 +1,8 @@
 ﻿namespace SortDeck
 {
+    /// <summary>
+    /// Implements visitor pattern for processing each card while iterating the deck.
+    /// </summary>
     public interface ICardProcessor
     {
         void Process(Card card);
